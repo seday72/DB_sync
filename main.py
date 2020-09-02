04b60d8af127adf360ef83ccea477371c0613611
seday72 <seday72@yandex.com>
2020-09-02 18:41:52 +0300

    test mongo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from setting import *
 
 if __name__ == '__main__':
     client = mongo_connect(mongoDB_url_test)
-    db = mongo_get_db(client, 'aaa')
+    db = mongo_get_db(client, mongoDB_test_dbname)
     collection_names = db.list_collection_names()
     for collection_name in collection_names:
         print(collection_name)

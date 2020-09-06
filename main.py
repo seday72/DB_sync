2942e71ba54336732c42ab1b68058200c8882385
Honey Dev <enjoydhoney@gmail.com>
2020-09-06 11:48:38 +0300

    get categories and it's assets

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ if __name__ == '__main__':
             }
             if 'assets' in collection_names:
                 category_asset = db['assets'].find_one({"_id": category['image']}, {"_id": 1, "url": 1, "type": 1})
+                print(category_asset)
                 if 'url' in category_asset and category_asset['url'] is not None:
                     print('category image url: {}'.format(category_asset['url']))
                     m_category['asset'] = category_asset
